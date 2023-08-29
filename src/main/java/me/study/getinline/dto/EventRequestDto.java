7bc15d0405c37f1d070517da67ac458180e31ec5
박종훈 <m05214@marketboro.com>
2023-08-29 13:58:06 +0900

    Add #2 - api dto files
    api에서 사용하는 DTO 파일 추가

---
+++ src/main/java/me/study/getinline/dto/EventRequestDto.java
@@ -0,0 +1,10 @@
+package me.study.getinline.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EventRequestDto {
+	private int eventId;
+}
---
+++ src/main/java/me/study/getinline/dto/PlaceRequestDto.java
@@ -0,0 +1,10 @@
+package me.study.getinline.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PlaceRequestDto {
+	private int placeId;
+}
